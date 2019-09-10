940031e30f876e9ee3e15cccec569c9d6607ffef
KKARTHIK <karthik.karthik@ncs.com.sg>
2019-09-10 09:50:43 +0800

    Comment 1 line

--- test/test/MyStringStackTest.java
+++ test/test/MyStringStackTest.java
@@ -85,7 +85,7 @@ public class MyStringStackTest extends TestCase {
 		String o1 = null;  
 		MyStringStack stack = new MyStringStack();
         assertTrue (stack.isEmpty());
-        stack.push (s1);
+        // stack.push (s1);
         stack.push (s2);
         assertFalse (stack.isEmpty());
         stack.clear ();
